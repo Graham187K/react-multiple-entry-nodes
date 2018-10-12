92093ca6145fb675e57d87bc0b163d85cde291db
Graham187K <graham187k@gmail.com>
2018-10-11 21:14:10 -0400

    Added multiple components

---
+++ src/components/Global.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class GlobalComponent extends Component {
+    render() {
+        return <div>Global Component</div>;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import GlobalComponent from "./components/Global";
+import Profiles from "./components/Profile";
+
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const globalComponent = document.getElementById("global-component");
+if (globalComponent) {
+    ReactDOM.render(<GlobalComponent />, globalComponent);
+}
+
+const profiles = document.getElementById("profile-component");
+if (profiles) {
+    ReactDOM.render(<Profiles />, profiles);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
